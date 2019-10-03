d61a1908294b65660967169b0d6a2bbabde64b6f
favorable-device-5158 <blee2125@gmail.com>
2019-10-03 21:40:36 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
 
+
+  @@all= []
+
+  def initialize(waiter, customer, total, tip)
+    @waiter= waiter
+    @customer= customer
+    @total= total
+    @tip= tip
+  end
+
+  def self.all
+    @@all
+  end
+  
 end
